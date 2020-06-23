0fc296bb78d4949f88a9d7b0d37cae333d3d392d
Silond <silond@gmail.com>
2020-06-23 11:24:38 -0500

    adding two new classes

---
+++ src/test/java/GitIntro.java
@@ -0,0 +1,17 @@
+public class GitIntro {
+
+    public static void main(String[] args) {
+        String original= "Corsan";
+        String reversed="";
+        for(int i=original.length()-1; i>=0; i--){
+            reversed=reversed+original.charAt(i);
+
+        }
+        System.out.println(reversed);
+        StringBuilder reversed1= new StringBuilder();
+        reversed1.append(reversed).reverse();
+        System.out.println(reversed1);
+    }
+
+
+}
---
+++ src/test/java/GitPracticeWithVova.java
@@ -0,0 +1,7 @@
+public class GitPracticeWithVova {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
