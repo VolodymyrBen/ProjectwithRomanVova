8f257b0abe9b163ddc2ac4b98340c4e25642bd54
HasanB-yte <65308505+HasanB-yte@users.noreply.github.com>
2020-06-23 12:06:49 -0500

    Update GitPracticeWithVova.java
    added two new lines

--- src/test/java/GitPracticeWithVova.java
+++ src/test/java/GitPracticeWithVova.java
@@ -4,6 +4,8 @@ public class GitPracticeWithVova {
 
         System.out.println("Hello World");
         System.out.println("Hello Roman and Vova");
+          System.out.println("Hello World");
+        System.out.println("Hello Roman and Vova");
         
     }
 }
